94bec388bb0c5d952897ba0654cd9d23236e07d4
Donald <donald@thehoefer.com>
2013-07-08 12:23:45 -0400

    Enter: the background task
    Async wutwut!!

--- PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,16 +1,23 @@
 package com.bignerdranch.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 /**
  * Created by panda on 7/8/13.
  */
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
+    private static final String URL = "http://www.google.com";
+
     GridView mGridView;
 
     @Override
@@ -18,6 +25,7 @@ public class PhotoGalleryFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
@@ -28,5 +36,18 @@ public class PhotoGalleryFragment extends Fragment {
         mGridView = (GridView)v.findViewById(R.id.gridView);
 
         return v;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrl(URL);
+                Log.i(TAG, "Fetched contents of URL: " + result);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Failed to fetch URL: " + ioe);
+            }
+            return null;
+        }
     }
 }
