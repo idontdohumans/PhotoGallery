452ecf18c1a592a465d03778cde2c759e59481d1
Donald <donald@thehoefer.com>
2013-07-10 19:26:11 -0400

    StartupReceiver now starts background polling alarm

--- PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
+++ PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
@@ -3,6 +3,8 @@ package com.bignerdranch.android.photogallery;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 /**
@@ -14,5 +16,9 @@ public class StartupReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean isOn = prefs.getBoolean(PollService.PREF_IS_ALARM_ON, false);
+        PollService.setServiceAlarm(context, isOn);
     }
 }
