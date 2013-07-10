dac51771039ecb8db37f037b49ad3eaa665aa7f4
Donald <donald@thehoefer.com>
2013-07-10 08:52:21 -0400

    pollService checks for new results based on id of current results.

--- PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -22,6 +22,7 @@ public class FlickrFetchr {
     public static final String TAG = "FlickrFetchr";
 
     public static final String PREF_SEARCH_QUERY    = "searchQuery";
+    public static final String PREF_LAST_RESULT_ID  = "lastResultId";
 
     private static final String ENDPOINT            = "http://api.flickr.com/services/rest/";
     private static final String API_KEY             = "867a3d1084b22d228adec9c3ddec38d7";
