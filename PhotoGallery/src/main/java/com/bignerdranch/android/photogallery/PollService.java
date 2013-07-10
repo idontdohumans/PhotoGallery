14e647aa49e1fe6f26efb4c861d5f6376383256f
Donald <donald@thehoefer.com>
2013-07-10 08:45:30 -0400

    Checking for network state.
    because, you wouldn't want to connect to the internet if you can't connect to the internet.

--- PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/PollService.java
+++ PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -1,7 +1,9 @@
 package com.bignerdranch.android.photogallery;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 
 /**
@@ -17,6 +19,13 @@ public class PollService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        @SuppressWarnings("deprecation")
+        boolean isNetworkAvailable = cm.getBackgroundDataSetting() &&
+                cm.getActiveNetworkInfo() != null;
+        if (!isNetworkAvailable) return;
+        
         Log.i(TAG, "Received an intent: " + intent);
     }
 }
