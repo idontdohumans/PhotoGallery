4e83cca73efbbe40f29c1a9b9ca4bdee9e7eff78
Donald <donald@thehoefer.com>
2013-07-11 09:05:53 -0400

    clicking on photos now opens webView with appropriate page for photo clicked.

--- PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
+++ PhotoGallery/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by panda on 7/11/13.
@@ -26,6 +27,16 @@ public class PhotoPageFragment extends VisibleFragment {
         View v = inflater.inflate(R.layout.fragment_photo_page, parent, false);
 
         mWebView = (WebView)v.findViewById(R.id.webView);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+
+        mWebView.loadUrl(mUrl);
 
         return v;
     }
